6f2e3794f658d6c9216a099269b13407582bb233
Rogerson Nazário <rrnazario@gmail.com>
2023-02-24 16:50:41 +0100

    Remove default implementations

--- src/Backend/ask-in-one-api/src/app.module.ts
+++ src/Backend/ask-in-one-api/src/app.module.ts
@@ -1,10 +1,35 @@
+import { classes } from '@automapper/classes';
+import { AutomapperModule } from '@automapper/nestjs';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './entities';
+import { WaiterModule } from './features/waiter/waiter.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AutomapperModule.forRoot({
+      strategyInitializer: classes(),
+  }),
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule,
+        WaiterModule,
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities,
+        migrations: ['./migrations/*.ts'],
+        //synchronize: true
+      }),        
+      inject: [ConfigService],
+    })
+  ],
 })
 export class AppModule {}
