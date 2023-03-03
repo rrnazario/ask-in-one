cdefd6439e1a95a93e1686dc9ebcbcd4bf723cc7
Rogerson Nazário <rrnazario@gmail.com>
2023-03-03 13:48:24 +0100

    [BE] Protect company controller

--- src/Backend/ask-in-one-api/src/features/company/company.controller.ts
+++ src/Backend/ask-in-one-api/src/features/company/company.controller.ts
@@ -1,15 +1,19 @@
 import { MapPipe } from '@automapper/nestjs';
 import { Body, Controller, HttpCode, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Put } from '@nestjs/common/decorators';
+import { Put, UseGuards } from '@nestjs/common/decorators';
 import { CommandBus } from '@nestjs/cqrs';
+import { UserType } from 'src/entities/user.entity';
+import { AllowedFor, JwtAuthGuard } from 'src/validations';
 import { AddCompanyCommand, AddCompanyRequest } from './add-company';
 
 @Controller('company')
+@AllowedFor(UserType.Admin)
+@UseGuards(JwtAuthGuard)
+@UsePipes(ValidationPipe)
 export class CompanyController {
     constructor(private readonly mediator: CommandBus) { }
 
     @Put()
-    @UsePipes(ValidationPipe)
     @HttpCode(201)
     async AddAsync(@Body(MapPipe(AddCompanyRequest, AddCompanyCommand)) cmd: AddCompanyCommand)
     : Promise<any> {
