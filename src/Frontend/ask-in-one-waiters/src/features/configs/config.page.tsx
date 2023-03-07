78a56e65577279c84fd30520c923e9d29ce0c268
Rogerson Nazário <rrnazario@gmail.com>
2023-03-07 19:07:05 +0100

    [FE] Add config and kitchen skeleton

---
+++ src/Frontend/ask-in-one-waiters/src/features/configs/config.page.tsx
@@ -0,0 +1,9 @@
+import Footer from "../../components/footer/footer.component";
+import AskTitle from "../../components/title/asktitle.component";
+
+export function Configurations(){
+    return <>
+        <AskTitle text="Configurações" />
+        <Footer />
+    </>
+}
---
+++ src/Frontend/ask-in-one-waiters/src/features/kitchen/kitchen-panel.page.tsx
@@ -0,0 +1,9 @@
+import Footer from "../../components/footer/footer.component";
+import AskTitle from "../../components/title/asktitle.component";
+
+export function KitchenPanel(){
+    return <>
+        <AskTitle text="Cozinha" />
+        <Footer />
+    </>
+}
