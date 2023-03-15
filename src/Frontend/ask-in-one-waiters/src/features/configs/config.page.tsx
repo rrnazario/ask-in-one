35a7dcb318aee099fd422d8b5ed285ce9f1ab7a9
Rogerson Nazário <rrnazario@gmail.com>
2023-03-15 16:58:57 +0100

    add logout button

--- src/Frontend/ask-in-one-waiters/src/features/configs/config.page.tsx
+++ src/Frontend/ask-in-one-waiters/src/features/configs/config.page.tsx
@@ -1,9 +1,41 @@
+import { Button } from "@mui/material";
 import Footer from "../../components/footer/footer.component";
 import AskTitle from "../../components/title/asktitle.component";
+import { unauthenticate } from "../../redux-ts";
+import { useAppDispatch } from "../../redux-ts/hooks";
 
-export function Configurations(){
+export function Configurations() {
+    const dispatch = useAppDispatch();
+
+    const onLogout = async () => {
+        //await setLogoutInfoVisible(false);
+
+        // const { data } = await service.logout({
+        //     login: String(localStorage.getItem('login')),
+        //     token: String(localStorage.getItem('token'))
+        // });
+
+        dispatch(unauthenticate());
+        
+        localStorage.removeItem('token');
+        localStorage.removeItem('login');
+
+        window.location.reload();
+    }
+
     return <>
         <AskTitle text="Configurações" />
+        <div style={{
+            display: 'flex',
+            width: '100%',
+            alignItems: 'center',
+            flexWrap: 'wrap'
+        }}>
+            <Button onClick={onLogout} color={'primary'}>
+                Deslogar
+            </Button>
+        </div>
+
         <Footer />
     </>
 }
