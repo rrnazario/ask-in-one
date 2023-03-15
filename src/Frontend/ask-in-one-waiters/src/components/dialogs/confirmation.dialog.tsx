09d77168ae92fdb3f00b2fa194a00d83ae1e291e
Rogerson Nazário <rrnazario@gmail.com>
2023-03-15 13:58:40 +0100

    Create confirm dialog for gen purposes

---
+++ src/Frontend/ask-in-one-waiters/src/components/dialogs/confirmation.dialog.tsx
@@ -0,0 +1,39 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import React, { useEffect } from "react";
+
+export interface ConfirmationDialogProps<T> {
+    item: T;
+    dialogText: string;
+    dialogTitle?: string;
+    open: boolean;
+
+    onClose: (item: T) => Promise<any>;
+    onConfirm: (item: T) => Promise<any>;
+}
+
+export function ConfirmationDialog<T>(props: ConfirmationDialogProps<T>) {
+    const { onClose, onConfirm, dialogText, dialogTitle, open } = props;
+
+    return (
+        <Dialog
+            sx={{ '& .MuiDialog-paper': { width: '80%', maxHeight: 435 } }}
+            maxWidth="xs"
+            open={open}
+        >
+            <DialogTitle>{dialogTitle ?? 'Atenção'}</DialogTitle>
+            <DialogContent dividers>
+                <DialogContentText>
+                    {dialogText}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={async () => await onConfirm(props.item)} color={'primary'}>
+                    Confirmar
+                </Button>
+                <Button autoFocus onClick={async () => await onClose(props.item)} color={'error'}>
+                    Cancelar
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
